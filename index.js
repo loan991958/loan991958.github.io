2501bf0faaa19c769a7429c25218104977280acc
loan991958 <120094463+loan991958@users.noreply.github.com>
2025-10-17 01:56:41 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 // can dat ben html cai nay de chay Swal
 //<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- Import SweetAlert2 -->
 
-//const keyboard1 = document.getElementById("keyboard1");
-//const keyboard2 = document.getElementById("keyboard2");
-
 function chatBoxKb1(){
     let bien1 = listLangCountry[indexSelect1Update];
     Swal.fire({
         title: `<span style='color:darkgreen;'>🗣️Chat by keyboard in ${bien1}</span>`,
         html: `
-            <textarea id="box1_ghi" style="border: 1px solid #ccc; 
-                                padding: 10px; 
-                                font-size:24px;
-                                width:100%;
-                                height: 300px; 
-                                overflow-y: scroll; 
-                                margin-bottom: 10px;">
-            </textarea>
+            <textarea id="box1_ghi" spellcheck="false"></textarea>
+            <script>
+                window.addEventListener('DOMContentLoaded', () => {
+                    document.getElementById('box1_ghi').focus();
+                });
+            </script>
             `,
         showCancelButton: true,
         confirmButtonText: "OK",
@@ -36,14 +31,12 @@ function chatBoxKb2(){
     Swal.fire({
         title: `<span style='color:darkblue;'>👤Chat by keyboard in ${bien2}</span>`,
         html: `
-            <textarea id="box2_ghi" style="border: 1px solid #ccc; 
-                                padding: 10px; 
-                                font-size:24px;
-                                width:100%;
-                                height: 300px; 
-                                overflow-y: scroll; 
-                                margin-bottom: 10px;">
-            </textarea>
+            <textarea id="box2_ghi" spellcheck="false"></textarea>
+            <script>
+                window.addEventListener('DOMContentLoaded', () => {
+                    document.getElementById('box2_ghi').focus();
+                });
+            </script>
             `,
         showCancelButton: true,
         confirmButtonText: "OK",
